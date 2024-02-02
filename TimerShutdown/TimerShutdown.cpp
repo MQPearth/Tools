26130b6bc16b6abe2bfaecb967bf34da3900e676
zzx <zzxjava@163.com>
2024-02-02 23:44:55 +0800

    QTimer

--- TimerShutdown/TimerShutdown.cpp
+++ TimerShutdown/TimerShutdown.cpp
@@ -1,13 +1,23 @@
 ﻿#include "TimerShutdown.h"
-
 #include <QLabel>
-#include "timer.h"
+#include <QString>
+#include "iostream"
 
-TimerShutdown::TimerShutdown(QWidget* parent) : QMainWindow(parent)
+
+TimerShutdown::TimerShutdown()
 {
 	ui.setupUi(this);
-	connect(ui.startButton, SIGNAL(clicked()), this, SLOT(clickButton()));
+
+	connect(ui.startButton, &QPushButton::clicked, this, &TimerShutdown::clickButton);
+
+	timer = new QTimer(this);
+
+	connect(timer, &QTimer::timeout, this, &TimerShutdown::doTimer);
+
+	timer->setInterval(1000);
 }
+
+
 
 void TimerShutdown::clickButton() {
 	int hour = ui.spinBoxHour->value();
@@ -23,30 +33,78 @@ void TimerShutdown::clickButton() {
 		second = 0;
 	}
 
-	//移除原有控件
 	ui.spinBoxHour->deleteLater();
 	ui.spinBoxMinute->deleteLater();
 	ui.spinBoxSecond->deleteLater();
 	ui.startButton->deleteLater();
-	//添加新控件
-	QLabel* timeLabel = new QLabel;
+
+	this->timeLabel = new QLabel;
+
+	this->timeLabel->setAlignment(Qt::AlignCenter);
+
 	QFont ft;
 	ft.setPointSize(56);
 	ft.setFamily(QString::fromUtf8("JetBrains Mono"));
 	timeLabel->setFont(ft);
 	ui.horizontalLayout->addWidget(timeLabel);
 
-	QPushButton* stopButton = new QPushButton("停止");
+	stopButton = new QPushButton("停止");
 	
 	ui.verticalLayout->addWidget(stopButton);
-	//开启新线程
-	this->timerThread = new TimerThread(hour, minute, second, timeLabel);
-	
-	this->timerThread->start();
 
-	connect(stopButton, SIGNAL(clicked()), this, SLOT(stopButton()));
+	connect(stopButton, &QPushButton::clicked, this, &TimerShutdown::handleStopButton);
+
+	totalSec = hour * 60 * 60 + minute * 60 + second;
+
+	updateLabel();
+
+	this->timer->start();
 }
 
-void TimerShutdown::stopButton() {
-	this->timerThread->terminate();
+void TimerShutdown::updateLabel() {
+	int hour = totalSec / 60 / 60;
+
+	int minute = (totalSec - 60 * 60 * hour) / 60;
+
+	int second = totalSec % 60;
+
+	QString hourStr = QString::number(hour);
+	if (hourStr.length() < 2) {
+		hourStr = "0" + hourStr;
+	}
+	QString minuteStr = QString::number(minute);
+	if (minuteStr.length() < 2) {
+		minuteStr = "0" + minuteStr;
+	}
+	QString secondStr = QString::number(second);
+	if (secondStr.length() < 2) {
+		secondStr = "0" + secondStr;
+	}
+
+	QString label = hourStr + ":" + minuteStr + ":" + secondStr;
+	this->timeLabel->setText(label);
+
+}
+
+void TimerShutdown::doTimer() {
+	totalSec--;
+
+	updateLabel();
+
+	if (!totalSec) {
+		std::cout << "te" << std::endl;
+		system("shutdown -s -f -t 0");
+	}
+}
+
+void TimerShutdown::handleStopButton() {
+	state = !state;
+	if (state) {
+		timer->stop();
+		stopButton->setText("开启");
+	}
+	else {
+		timer->start();
+		stopButton->setText("停止");
+	}
 }
--- TimerShutdown/TimerShutdown.h
+++ TimerShutdown/TimerShutdown.h
@@ -2,21 +2,35 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_TimerShutdown.h"
-#include "timer.h"
+#include <QTimer>
+#include <QLabel>
 
 class TimerShutdown : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    TimerShutdown(QWidget *parent = Q_NULLPTR);
+    TimerShutdown();
+
+    void updateLabel();
 public slots:
     void clickButton();
 
-    void stopButton();
+    void handleStopButton();
+
+    void doTimer();
 
 private:
-    TimerThread* timerThread;
+    int hour = 0;
+    int minute = 0;
+    int second = 0;
+    bool state = false;
+
+    QTimer* timer = nullptr;
+
+    int totalSec = 0;
 
     Ui::TimerShutdownClass ui;
+    QPushButton* stopButton = nullptr;
+    QLabel* timeLabel = nullptr;
 };
